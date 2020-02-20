75c3ebec2aa1d5beabef48ebc566bb5c7e0cc37a
HyeN123 <tiqmffk123@gmail.com>
2020-02-20 17:58:06 +0900

    포스트 기능 추가

--- src/main/java/com/example/spring/web/RestWebController.java
+++ src/main/java/com/example/spring/web/RestWebController.java
@@ -1,13 +1,26 @@
 package com.example.spring.web;
 
+import com.example.spring.domain.dto.posts.PostsSaveRequestDto;
+import com.example.spring.domain.posts.PostsRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class RestWebController {
 
-    @GetMapping
+    private PostsRepository postsRepository;
+
+    @GetMapping("/hello")
     public String hello(){
         return "Hello, World";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+        postsRepository.save(dto.toEntity());
     }
 }
