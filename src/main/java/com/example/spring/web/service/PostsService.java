894b0878de0ca1ba9703b4f0800a312e613e91a6
박민우 <minwoo@makestar.com>
2020-03-09 12:52:42 +0900

    게시글 목록 불러오기 기능

--- src/main/java/com/example/spring/web/service/PostsService.java
+++ src/main/java/com/example/spring/web/service/PostsService.java
@@ -1,19 +1,32 @@
 package com.example.spring.web.service;
 
+import com.example.spring.domain.dto.posts.PostsMainResponseDto;
 import com.example.spring.domain.dto.posts.PostsSaveRequestDto;
 import com.example.spring.domain.posts.PostsRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 @AllArgsConstructor
 @Service
 public class PostsService {
+
     private PostsRepository postsRepository;
 
     @Transactional
     public Long save(PostsSaveRequestDto dto){
         return postsRepository.save(dto.toEntity()).getId();
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsMainResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc()
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
     }
 
 }
